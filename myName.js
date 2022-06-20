2f32151c1471d7bcaaf8a84e7d03aadf36048464
wisdom osagbemeh <wisdomosagbemeh@gmail.com>
2022-06-20 20:42:04 +0100

    Javascript intro

---
+++ myName.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+document.writeln('Name: Wisdom Osagbemeh');
+document.write("<br/>")
+document.writeln('Height: 180cm');
+document.write("<br/>")
+document.writeln('Country: Nigeria');
